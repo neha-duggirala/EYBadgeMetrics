27c239f658f161aacb5d4f8996375cbc3e169f33
neha-duggirala <nsm.duggirala@gmail.com>
2020-08-28 00:20:37 +0530

    for one record of employee JSON is aheived

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using EYBadges.Models;
+using EYBadges.Dto;
 
 namespace EYBadges.Controllers
 {
@@ -37,13 +38,54 @@ namespace EYBadges.Controllers
             }
 
             var employee = await _context.Employee.FindAsync(id);
+            var teamId = employee.TeamId;
+            var teamDetails = await _context.TeamDetails.FindAsync(teamId);
+
+            //TODO: find all list of dates with same employee ID
+            var outOfTheBox = await _context.OutOfTheBox.FindAsync(id);
+
+            EmployeeDto response = new EmployeeDto();
+
+            // if employee is Dev
+            if (employee.EmployeeType == 1)
+            {
+                //TODO: find all list of dates with same employee ID
+                var developerKpi = await _context.DeveloperKpi.FindAsync(id);
+
+                DeveloperKpiDto devDto = new DeveloperKpiDto();
+
+                devDto.Date = developerKpi.Date;
+                devDto.TestCoverage = developerKpi.TestCoverage;
+                devDto.CodeQualityPercent = developerKpi.CodeQualityPercent;
+                devDto.CodeSmellPercent = developerKpi.CodeSmellPercent;
+                devDto.Kloc = developerKpi.Kloc;
+                devDto.Throughput = developerKpi.Throughput; 
+                response.DeveloperKpiDto = devDto;
+            }
+
+            //TODO: if employee is Tester
+
+            OutOfBoxDto outOfTheBoxDto = new OutOfBoxDto();
+            outOfTheBox.Date = outOfTheBox.Date;
+            outOfTheBox.IdeasToEnhanceTheCompany = outOfTheBox.IdeasToEnhanceTheCompany;
+            outOfTheBox.IdeasToEnhanceTheProject = outOfTheBox.IdeasToEnhanceTheProject;
+
+            
+            response.EmployeeId = id;
+            response.EmployeeName = employee.EmployeeName;
+            response.TeamName = teamDetails.TeamName;
+            //response.DeveloperKpiDto.Date = developerKpi.Date;
+            
+            response.OutOfBoxDto = outOfTheBoxDto;
+
+
 
             if (employee == null)
             {
                 return NotFound();
             }
 
-            return Ok(employee);
+            return Ok(response);
         }
 
 
