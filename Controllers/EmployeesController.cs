2535b1d6926aef4e3655a1fc3ee0ad7837e55a01
neha-duggirala <nsm.duggirala@gmail.com>
2020-08-28 01:42:48 +0530

    Added list of outofboxdtos to Employee DTO

--- Controllers/EmployeesController.cs
+++ Controllers/EmployeesController.cs
@@ -40,43 +40,52 @@ namespace EYBadges.Controllers
             var employee = await _context.Employee.FindAsync(id);
             var teamId = employee.TeamId;
             var teamDetails = await _context.TeamDetails.FindAsync(teamId);
-
-            //TODO: find all list of dates with same employee ID
-            var outOfTheBox = await _context.OutOfTheBox.FindAsync(id);
-
             EmployeeDto response = new EmployeeDto();
+            //find all list of dates with same employee ID
+            var outOfTheBox =  _context.OutOfTheBox.FromSql("select * from OutOfTheBox where EmployeeId = {0}", id).ToList();
+            
+            var outOfBoxDtoList = new List<OutOfBoxDto>();
+            foreach (var item in outOfTheBox)
+            {
+                OutOfBoxDto outOfTheBoxDto = new OutOfBoxDto();
+                outOfTheBoxDto.Date= item.Date;
+                outOfTheBoxDto .IdeasToEnhanceTheCompany= item.IdeasToEnhanceTheCompany;
+                outOfTheBoxDto.IdeasToEnhanceTheProject= item.IdeasToEnhanceTheProject;
+                outOfBoxDtoList.Add(outOfTheBoxDto);
+
+            }
+
+            response.OutOfBoxDto = outOfBoxDtoList;
+
 
             // if employee is Dev
             if (employee.EmployeeType == 1)
             {
                 //TODO: find all list of dates with same employee ID
-                var developerKpi = await _context.DeveloperKpi.FindAsync(id);
-
+                //var developerKpi = await _context.DeveloperKpi.FindAsync(id);
+                var developerKpi =  _context.DeveloperKpi.FromSql("select * from DeveloperKpi where EmployeeId = {0}",id).ToList();
+                Console.WriteLine(developerKpi);
                 DeveloperKpiDto devDto = new DeveloperKpiDto();
 
-                devDto.Date = developerKpi.Date;
-                devDto.TestCoverage = developerKpi.TestCoverage;
-                devDto.CodeQualityPercent = developerKpi.CodeQualityPercent;
-                devDto.CodeSmellPercent = developerKpi.CodeSmellPercent;
-                devDto.Kloc = developerKpi.Kloc;
-                devDto.Throughput = developerKpi.Throughput; 
-                response.DeveloperKpiDto = devDto;
+                //devDto.Date = developerKpi.Date;
+                //devDto.TestCoverage = developerKpi.TestCoverage;
+                //devDto.CodeQualityPercent = developerKpi.CodeQualityPercent;
+                //devDto.CodeSmellPercent = developerKpi.CodeSmellPercent;
+                //devDto.Kloc = developerKpi.Kloc;
+                //devDto.Throughput = developerKpi.Throughput; 
+                //response.DeveloperKpiDto = devDto;
             }
 
             //TODO: if employee is Tester
 
-            OutOfBoxDto outOfTheBoxDto = new OutOfBoxDto();
-            outOfTheBox.Date = outOfTheBox.Date;
-            outOfTheBox.IdeasToEnhanceTheCompany = outOfTheBox.IdeasToEnhanceTheCompany;
-            outOfTheBox.IdeasToEnhanceTheProject = outOfTheBox.IdeasToEnhanceTheProject;
-
+            
             
             response.EmployeeId = id;
             response.EmployeeName = employee.EmployeeName;
             response.TeamName = teamDetails.TeamName;
             //response.DeveloperKpiDto.Date = developerKpi.Date;
             
-            response.OutOfBoxDto = outOfTheBoxDto;
+
 
 
 
